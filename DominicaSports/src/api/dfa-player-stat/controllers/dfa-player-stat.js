a0b377073f990cc54f191b501febec70af09d2c2
Danphil Daniel <danieldanphil@gmail.com>
2024-02-14 21:42:33 -0400

    Update to player stat schema

---
+++ DominicaSports/src/api/dfa-player-stat/controllers/dfa-player-stat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dfa-player-stat controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dfa-player-stat.dfa-player-stat');
